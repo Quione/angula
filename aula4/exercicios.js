832a90dc1fe87aa7620085fc3f25bc2c85ac3579
Quione <40217639+Quione@users.noreply.github.com>
2019-02-13 21:33:52 -0200

    aula4

---
+++ aula4/exercicios.js
@@ -0,0 +1,26 @@
+//exerc.1
+var preco = 150;
+var desconto = parseFloat('9');
+var valorDesc = preco * (desconto/100);
+var novoPreco = preco - valorDesc;
+console.log(novoPreco);
+
+//exerc.2
+
+//exerc.3
+
+//exerc.4
+var salario = 800;
+var comissao = 300;
+var numCarros = 3;
+var salarioTotal = comissao * numCarros + salario;
+console.log(salarioTotal);
+
+//exerc.5
+
+var eleitoresMun = 10000;
+var votosNull = 5000;
+var votosBran = 2000;
+var votosVal = 3000;
+
+
